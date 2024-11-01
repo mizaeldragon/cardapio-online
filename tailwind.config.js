8ea479778d559970e1a434455e5660f6d955f8af
mizaeldragon <raidechin@gmail.com>
2024-11-01 00:40:07 -0300

    return hora >= 18 && hora < 25;

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    fontFamily: { sans: ["Poppins", "sans-serif"] },
+    extend: {
+      backgroundImage: {
+        home: "url('/assets/bg.webp')",
+      },
+    },
+  },
+  plugins: [],
+};
